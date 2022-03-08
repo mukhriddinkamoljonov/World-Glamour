ad685ab334cb4bd4f3f5a72dc0964af48480496e
Muhriddin Kamoljonov <muhriddinkamoljonov@gmail.com>
2022-03-08 22:10:55 +0500

    footer changed

--- src/Components/Basic Page/Footer.jsx
+++ src/Components/Basic Page/Footer.jsx
@@ -41,7 +41,7 @@ const Footer = () => {
                 <ul className="footer-adress">
                   <li>
                     <i className="pe-7s-map-marker strong"> </i> Farg'ona
-                    shahar, Birodarlik 28 uy
+                    shahar, Al-Marg'iloniy 12 uy.
                   </li>
                   <li>
                     <i className="pe-7s-mail strong"> </i> rnj-787@mail.ru
@@ -132,7 +132,7 @@ const Footer = () => {
                 <div className="footer-title-line"></div>
                 <p>Barcha xuquqlar himoyalangan. Batafsil ma'lumot uchun:</p>
 
-                <p>(998) 90 353-01-04</p>
+                <p>(998) 93 643-93-11</p>
 
                 <form>
                   <div className="input-group">
