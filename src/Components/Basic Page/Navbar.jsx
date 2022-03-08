9cf92084ef43d09c9c15d11c85c85ee5a7f1a220
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-03-08 22:09:29 +0500

    Navbar trasparent not problem

--- src/Components/Basic Page/Navbar.jsx
+++ src/Components/Basic Page/Navbar.jsx
@@ -91,14 +91,11 @@ const Navbar = ({ setQuery }) => {
                 </Link>
                 <ul className="dropdown-menu navbar-nav">
                   <li>
-
                     <Link to="/chorvoq">Oromgohlar</Link>
                   </li>
                   <li>
                     <Link to="/chorvoq">O'zbekiston shaharlari</Link>
-
                   </li>
-                 
                   <li>
                     <Link to="/chimyon">Osiyo shaharlari</Link>
                   </li>
