c54de4c461ebacae1b1517d65f59de6487b412be
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-03-11 22:00:06 +0500

    Ishlab ishlamay ishlayabdi

--- src/App.js
+++ src/App.js
@@ -9,36 +9,15 @@ import AllCart from "./Components/Pages/AllCart";
 import Footer from "./Components/Basic Page/Footer";
 import Shaharlar from "./Components/Pages/Shaharlar";
 import React, { useState } from "react";
-import i18n from "i18next";
-import { useTranslation, initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import HttpApi from "i18next-http-backend";
-
-i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .use(LanguageDetector)
-  .use(HttpApi)
-  .init({
-    fallbackLng: "en",
-    detection: {
-      order: ["path", "cookie", "localStorage", "htmlTag"],
-      caches: ["cookie"],
-    },
-    backend: {
-      loadPath: "/assets/locales/{{lng}}/translation.json",
-    },
-    react: { useSuspense: false },
-  });
 
 function App() {
-  const { t } = useTranslation();
   const [query, setQuery] = useState("");
 
   return (
     <div>
       <Navbar setQuery={setQuery} />
-      <h2>{t("welcome_to_react")}</h2>;
       <Routes>
+        {" "}
         <Route path="/" element={<Home query={query} />} />
         <Route path="/sayyohlar" element={<Sayyohlar />} />
         <Route path="/about" element={<AboutWe />} />
--- src/Components/Basic Page/Main.js
+++ src/Components/Basic Page/Main.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import plane from "../icons/plane.png";
 import car from "../icons/car.png";
 import hotel from "../icons/hotel.png";
 import globe from "../icons/phone.png";
-import NoImage from "../../img/no-image.jpg";
 import dollar from "../icons/dollar.png";
 import rubl from "../icons/ruble.png";
 import evro from "../icons/evro.png";
@@ -13,9 +11,29 @@ import lir from "../icons/lira.png";
 import agreed from "../icons/agreed.png";
 import visa from "../icons/visa.png";
 import SmallCard from "./SmallCard";
-import Carts from "./Carts";
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    fallbackLng: "en",
+    detection: {
+      order: ["path", "cookie", "localStorage", "htmlTag"],
+      caches: ["cookie"],
+    },
+    backend: {
+      loadPath: `/assets/locales/{{lng}}/translation.json`,
+    },
+    react: { useSuspense: false },
+  });
 
 const Main = ({ query }) => {
+  const { t } = useTranslation();
   const [items, setItems] = useState([]);
   const [isReady, setIsReady] = useState(null);
   const [ruble, setRuble] = useState([]);
@@ -33,37 +51,21 @@ const Main = ({ query }) => {
         console.log(error);
       });
   }, []);
-  console.log(items);
+
   return (
     <div>
       {/* SLIDER AREA START */}
       <div className="slider-area">
-        {/* <div className="slider">
-          <div id="bg-slider" className="owl-carousel owl-theme">
-            <div className="item">
-              <img src="assets/img/slide1/sohil1.jpg" alt="GTA V" />
-            </div>
-            <div className="item">
-              <img
-                src="assets/img/slide1/sohil2.jpg"
-                alt="The Last of"
-              />
-            </div>
-            <div className="item">
-              <img src="assets/img/slide1/sohil3.jpg" alt="GTA V" />
-            </div>
-          </div>
-        </div> */}
         <div className="slider-content">
           <div className="row">
             <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12">
               <h2 className="header-title-per">
-                Sayohatni biz bilan mazmunli o'tkazing!
+                {t("Sayohatni biz bilan mazmunli o'tkazing!")}
               </h2>
               <h4 className="header-title-simple">
-                Eʼtiborga molik joylarni, insonlar hayoti va hodisalar
-                to‘g‘risida eng qiziqarli maʼlumotlarni biz orqali topishingiz
-                mumkin!
+                {t(
+                  "Eʼtiborga molik joylarni, insonlar hayoti va hodisalar to‘g‘risida eng qiziqarli maʼlumotlarni biz orqali topishingiz mumkin!"
+                )}
               </h4>
             </div>
           </div>
